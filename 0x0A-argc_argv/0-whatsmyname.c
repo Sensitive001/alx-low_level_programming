80301d89fa1f3cd55f21b4e00564524627f67690
Sensitive001 <adeboyeonyeka@gmail.com>
2022-11-04 03:00:25 -0700

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the name of the program
+ * @argc: count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+
+return (0);
+}
