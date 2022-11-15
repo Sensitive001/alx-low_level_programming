90bb3ad499c1eaed15afb4fe1125ba1f25cc9b04
Sensitive001 <adeboyeonyeka@gmail.com>
2022-11-14 21:45:46 -0800

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,7 @@ struct dog
 	char *owner;
 };
 
-typed struct dog dog_t;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
