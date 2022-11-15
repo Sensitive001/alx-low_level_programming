34e81caa41f1d1e3aa5419851b7319524efd7c66
Sensitive001 <adeboyeonyeka@gmail.com>
2022-11-14 22:15:57 -0800

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,16 +2,16 @@
 #define DOG_H
 
 /**
- * struct dog - struct dog
+* struct dog - Struct dog
  * @name: name
  * @age: age
  * @owner: owner
- * Description: This struct is for dog 
+ * Description: This struct is for dog
  */
 
 struct dog
-(
- 	char *name
+{
+	char *name;
 	float age;
 	char *owner;
 };
