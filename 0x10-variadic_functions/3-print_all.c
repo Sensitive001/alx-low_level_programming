057012004a63eb4d4b3957c7ee649e941f91e3bb
Sensitive001 <adeboyeonyeka@gmail.com>
2022-11-17 15:16:57 -0800

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -31,7 +31,7 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 		case 'c':
-			printf("%C", va_arg(valist, int)), c = 1;
+			printf("%c", va_arg(valist, int)), c = 1;
 			break;
 		case 'i':
 			printf("%d", va_arg(valist, int)), c = 1;
