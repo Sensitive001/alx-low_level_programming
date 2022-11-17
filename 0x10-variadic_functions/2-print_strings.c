0f7c8e6051fba17013c38ea4217075bf1bc6cc16
Sensitive001 <adeboyeonyeka@gmail.com>
2022-11-17 14:45:13 -0800

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,7 +13,7 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
-	unsigned int;
+	unsigned int i;
 	char *str;
 
 	va_start(valist, n);
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format: a list of types of arguments passed to the function
+ *
+ * Return: no return
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	unsigned int i = 0, j, c = 0;
+	char *str;
+	const char  t_arg[] = "cifs";
+
+	va_start(valist, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (t_arg[j])
