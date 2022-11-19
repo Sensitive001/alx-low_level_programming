15d05805f64671899c8a6469f90bcca3d5ed5e64
Sensitive001 <adeboyeonyeka@gmail.com>
2022-11-18 21:28:33 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - print _putchar
+ * main - Entry point
  *
- * Return: 0
+ * Return: Always 0 (Sucess)
  */
 
 int main(void)
 {
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
