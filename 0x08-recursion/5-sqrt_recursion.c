02e3817d725a17fdc88275916ba73f45c04a55cc
Sensitive001 <adeboyeonyeka@gmail.com>
2022-11-26 03:08:26 -0800

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -22,3 +22,22 @@ return (-1);
 
 return (-1);
 }
+
+/**
+ * _sqrt_recursion - evaluate sqrt
+ * @n: integer
+ * Return: sgrt_recursion
+ */
+int _sqrt_recursion(int n)
+{
+int i = 0;
+
+if (i <  0) /*if n is negative*/
+return (-1);
+
+else
+{
+return (_evaluate(i, n)); /*Recursive call*/
+}
+
+}
