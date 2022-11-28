2b31efd2284f84a8a2f208b9b30174affbf86832
Sensitive001 <adeboyeonyeka@gmail.com>
2022-11-27 20:58:40 -0800

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry - point
+ *
+ * Return: ALways 0 (sucess)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
