ea2b2824cded7652d37289f9b4b4d9e451bac78d
Sensitive001 <adeboyeonyeka@gmail.com>
2022-11-28 13:57:05 -0800

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,11 @@
-#Include "lists.h"
+#include "lists.h"
 #include <stdio.h>
 
 /**
- * print_listint - prints all the elemnts of a listint_t list
- * @h: A pointer to the head of all the list_t list
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: A pointer to the head of the list_t list
  *
- * Return: the number of nodes in the list_t list
+ * Return: The number of nodes in the list_t list.
  */
 size_t print_listint(const listint_t *h)
 {
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,7 +6,7 @@
 /**
  * struct listint_s - singly linked list
  * @n: integer
- * @next: points to the next code
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
  * for project
@@ -34,4 +34,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif /*LISTS_H*/
+#endif /* LISTS_H */
