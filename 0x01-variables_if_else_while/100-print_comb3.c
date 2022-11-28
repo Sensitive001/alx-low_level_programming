be678e4fb7e6d9c0f31497e034dd09719e976b50
Sensitive001 <adeboyeonyeka@gmail.com>
2022-11-27 21:33:31 -0800

    p comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,7 +11,7 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = '0'; tens <= '0'; tens++)/*prints tens digit*/
+	for (tens = '0'; tens <= '9'; tens++)/*prints tens digit*/
 	{
 		for (ones = '0'; ones <= '9'; ones++)/*prints one digit*/
 		{
