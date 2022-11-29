a39ddddcb8068dbb1778d9f11f3851544344de12
Sensitive001 <adeboyeonyeka@gmail.com>
2022-11-29 06:05:25 -0800

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
 int a, b;
-if (argc == 1)
+if (argc == 3)
 {
 a = atoi(argv[1]);
 b = atoi(argv[2]);
@@ -21,4 +21,3 @@ return (0);
 printf("Error\n");
 return (1);
 }
-
